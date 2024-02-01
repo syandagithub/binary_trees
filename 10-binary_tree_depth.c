0f0c9fcae70c7927ef56039165e54ce10f074a9d
SYANDA MNTAMBO's GITHUB <syandamnt@gmail.com>
2024-02-01 21:25:46 +0200

    binary_trees

---
+++ 10-binary_tree_depth.c
@@ -0,0 +1,18 @@
+#include "binary_trees.h"
+
+
+size_t binary_tree_depth(const binary_tree_t *tree)
+{
+	size_t depth = 0;
+
+	if (!tree)
+		return (0);
+
+	while (tree->parent)
+	{
+		depth++;
+		tree = tree->parent;
+	}
+
+	return (depth);
+}
---
+++ 13-binary_tree_nodes.c
@@ -0,0 +1,28 @@
+#include "binary_trees.h"
+
+
+size_t binary_tree_nodes(const binary_tree_t *tree)
+{
+	if (!tree || (!tree->left && !tree->right))
+		return (0);
+
+	return (binary_tree_size(tree) - binary_tree_leaves(tree));
+}
+
+size_t binary_tree_size(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	return (binary_tree_size(tree->left) + binary_tree_size(tree->right) + 1);
+}
+size_t binary_tree_leaves(const binary_tree_t *tree)
+{
+	if (!tree)
+		return (0);
+
+	if (!tree->left && !tree->right)
+		return (1);
+
+	return (binary_tree_leaves(tree->left) + binary_tree_leaves(tree->right));
+}
